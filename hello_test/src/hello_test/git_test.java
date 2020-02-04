fad7de5d40845f399ead0644fffa679d79982570
King Kong <cuteh@LAPTOP-QM0H46RJ.mynet>
2020-02-04 17:45:34 +0200

    v1-added mul function

--- hello_test/src/hello_test/git_test.java
+++ hello_test/src/hello_test/git_test.java
@@ -1,9 +1,13 @@
 package hello_test;
 
 public class git_test {
+	public static int mul(int x,int y) {
+		return x*y;
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int res = git_test.mul(10, 5);
 
 	}
 
